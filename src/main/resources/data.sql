e528e2a9002b4152f19ddf077f385d52304830db
ankitha <ankitha.codes@gmail.com>
2022-02-25 13:00:58 +0530

    Authentication Overview: updated DB data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO UserData (id, username, password,role) VALUES
   (1,'Ram','Ram','ROLE_USER,ROLE_ADMIN'),
-  (2,'Adithya', 'Adithya','ROLE_ADMIN'),
-  (3,'Ankitha', 'Ankitha','ROLE_ADMIN'),
+  (2,'Rahul', 'Rahul','ROLE_ADMIN'),
+  (3,'Hari', 'Hari','ROLE_ADMIN'),
   (4,'Sagar', 'Sagar','ROLE_EDITOR'),
   (5,'Laxman', 'Laxman','ROLE_PUBLIC');
 
